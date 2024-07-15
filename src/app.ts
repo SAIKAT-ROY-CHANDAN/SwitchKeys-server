1f709ca071b93b5bf742fe8d5c4b92f9f077b01a
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-07-15 18:07:00 +0600

    changes maded in server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import router from './app/router';
+import globalErrorHandlers from './app/middlewares/globalErrorHandlers';
 
 
 export const app: Application = express();
 
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use('/api', router)
 
 const test = (req: Request, res: Response) => {
-  const a = 'Hello from the Nexus Workspace'
+  const a = 'Hello from the Switch'
   res.send(a)
 }
 
 app.get('/', test)
+app.use(globalErrorHandlers)
 
 export default app
